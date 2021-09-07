9ba547c6f2e43266a4c11890e28240c89c3a4cf5
guutong <pornmongkonp@gmail.com>
2021-09-07 22:43:56 +0700

    fix: :fire: ico text spacing

--- projects/ngx-flex-messages/src/lib/utils.ts
+++ projects/ngx-flex-messages/src/lib/utils.ts
@@ -274,8 +274,9 @@ export default class Utils {
     customClass.push(position === 'absolute' ? 'ExAbs' : '');
 
     if (!(margin && margin.indexOf('px') >= 0)) {
-      customClass.push(margin ? `ExMgnT${this.upperAllDigit(margin)}` : '');
+      customClass.push(margin ? `ExMgnL${this.upperAllDigit(margin)}` : '');
     }
+
     if (offsetTop && offsetTop.indexOf('px') < 0) {
       customClass.push(offsetTop ? 'ExT' + this.upperAllDigit(offsetTop) : '');
     }
--- stories/NgxFlexMessages.stories.ts
+++ stories/NgxFlexMessages.stories.ts
@@ -84,4 +84,86 @@ export const NgxFlexMessagesExample12 = Template.bind({});
 NgxFlexMessagesExample12.args = {
   data: { "type": "flex", "altText": "Flex Message", "contents": { "type": "bubble", "hero": { "type": "image", "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_3_movie.png", "size": "full", "aspectRatio": "20:13", "aspectMode": "cover", "action": { "type": "uri", "uri": "http://linecorp.com/" } }, "body": { "type": "box", "layout": "vertical", "spacing": "md", "contents": [{ "type": "text", "text": "BROWN'S ADVENTURE\nIN MOVIE", "wrap": true, "weight": "bold", "gravity": "center", "size": "xl" }, { "type": "box", "layout": "baseline", "margin": "md", "contents": [{ "type": "icon", "size": "sm", "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png" }, { "type": "icon", "size": "sm", "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png" }, { "type": "icon", "size": "sm", "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png" }, { "type": "icon", "size": "sm", "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png" }, { "type": "icon", "size": "sm", "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png" }, { "type": "text", "text": "4.0", "size": "sm", "color": "#999999", "margin": "md", "flex": 0 }] }, { "type": "box", "layout": "vertical", "margin": "lg", "spacing": "sm", "contents": [{ "type": "box", "layout": "baseline", "spacing": "sm", "contents": [{ "type": "text", "text": "Date", "color": "#aaaaaa", "size": "sm", "flex": 1 }, { "type": "text", "text": "Monday 25, 9:00PM", "wrap": true, "size": "sm", "color": "#666666", "flex": 4 }] }, { "type": "box", "layout": "baseline", "spacing": "sm", "contents": [{ "type": "text", "text": "Place", "color": "#aaaaaa", "size": "sm", "flex": 1 }, { "type": "text", "text": "7 Floor, No.3", "wrap": true, "color": "#666666", "size": "sm", "flex": 4 }] }, { "type": "box", "layout": "baseline", "spacing": "sm", "contents": [{ "type": "text", "text": "Seats", "color": "#aaaaaa", "size": "sm", "flex": 1 }, { "type": "text", "text": "C Row, 18 Seat", "wrap": true, "color": "#666666", "size": "sm", "flex": 4 }] }] }, { "type": "box", "layout": "vertical", "margin": "xxl", "contents": [{ "type": "spacer" }, { "type": "image", "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/linecorp_code_withborder.png", "aspectMode": "cover", "size": "xl" }, { "type": "text", "text": "You can enter the theater by using this code instead of a ticket", "color": "#aaaaaa", "wrap": true, "margin": "xxl", "size": "xs" }] }] } } }
 };
-   
+
+export const NgxFlexMessagesExample13 = Template.bind({});
+NgxFlexMessagesExample13.args = {
+  data: {
+    "type": "flex",
+    "altText": "Flex Message",
+    "contents": {
+      "type": "bubble",
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "xxs Test test",
+            "weight": "bold",
+            "size": "xxs"
+          },
+          {
+            "type": "text",
+            "text": "xs Test test",
+            "weight": "bold",
+            "size": "xs",
+            "margin": "xs"
+          },
+          {
+            "type": "text",
+            "text": "sm Test test",
+            "weight": "bold",
+            "size": "sm",
+            "margin": "sm"
+          },
+          {
+            "type": "text",
+            "text": "md Test test",
+            "weight": "bold",
+            "size": "md",
+            "margin": "md"
+          },
+          {
+            "type": "text",
+            "text": "lg Test test",
+            "weight": "bold",
+            "size": "lg",
+            "margin": "lg"
+          },
+          {
+            "type": "text",
+            "text": "xl Test test",
+            "weight": "bold",
+            "size": "xl",
+            "margin": "xl"
+          },
+          {
+            "type": "text",
+            "text": "xxl Test test",
+            "weight": "bold",
+            "size": "xxl",
+            "margin": "xxl"
+          },
+          {
+            "type": "text",
+            "text": "3xl Test test ",
+            "weight": "bold",
+            "size": "3xl"
+          },
+          {
+            "type": "text",
+            "text": "4xl Test test",
+            "weight": "bold",
+            "size": "4xl"
+          },
+          {
+            "type": "text",
+            "text": "5xl Test test",
+            "weight": "bold",
+            "size": "5xl"
+          }
+        ]
+      }
+    }
+  }
+};
